ba8a0ea41da2796d07bd9685da57553af2a79274
Khalifa Mamadou NIAMADIO <khalifaniamadio@gmail.com>
2021-04-12 23:18:24 +0000

    ajout d'un form avec une drop list. Requête à modifier plus tard pour l'insertion dans la base de données

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import json
 import pymysql
-
+from tkinter import *
 
 file = "C:/Users/Main/MERN/test.json"
 json_data = open(file).read()
 json_obj = json.loads(json_data)
 
+OPTIONS = [
+    "2013",
+    "2014",
+    "2017"
+]  
+
+master = Tk()
+
+variable = StringVar(master)
+variable.set(OPTIONS[0])  # default value
+
+w = OptionMenu(master, variable, *OPTIONS)
+w.pack()
+
+
+def ok():
+    print("value is:" + variable.get())
+
+
+button = Button(master, text="OK", command=ok)
+button.pack()
 
 
 def validate_string(val):
@@ -22,12 +43,14 @@ def validate_string(val):
 con = pymysql.connect(host='localhost', user='root', passwd='', db='testjson')
 cursor = con.cursor()
 
-
 for i, item in enumerate(json_obj):
     person = validate_string(item.get("person", None))
     year = validate_string(item.get("year", None))
     company = validate_string(item.get("company", None))
 
-    cursor.execute("INSERT INTO testp (person,	year,	company) VALUES (%s,	%s,	%s)", (person, year, company))
+
+    cursor.execute("INSERT INTO testp (person,	year,	company) VALUES (%s,	%s,	%s) WHERE year= "+variable.get(),
+                   (person, year, company))
 con.commit()
 con.close()
+mainloop()
