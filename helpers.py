534b84909df0cd4d2eb99c70481e0a6360cb5a85
kheuch <cheikhseck980@gmail.com>
2021-05-06 13:15:02 +0000

    adding helper file

---
+++ helpers.py
@@ -0,0 +1,18 @@
+import re
+def nbCas_per_loc(text):
+    textArr = re.split(',',text)
+    notAllowed = [",","à","aux","",";"]
+    textArr = [i.strip().replace('.','') for i in textArr if i not in notAllowed]
+    nbCas = textArr[0].split(" ")[0]
+    regions = textArr[1:]
+    myDict = {}
+    for r in regions:
+        myDict[r]= nbCas
+    myDict[textArr[0].split(" ")[-1]] = nbCas
+    return myDict
+
+text = "01 à reubeuss, fann residence, lib 6, ouest foire."
+
+dico = nbCas_per_loc(text)
+
+print(dico)
