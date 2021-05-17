2d10d39039fa70d688a228a91f8451ed3c6c1b1a
Khalifa Mamadou NIAMADIO <khalifaniamadio@gmail.com>
2021-05-17 05:16:06 +0000

    ajout d'une interface utilisateur

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
 import pymysql
-from tkinter import *
 
 file = "C:/Users/Main/MERN/test.json"
 json_data = open(file).read()
@@ -17,20 +16,50 @@ def validate_string(val):
             return val
 
 
-# connect to MySQL
-con = pymysql.connect(host='localhost', user='root', passwd='', db='testjson')
-cursor = con.cursor()
+def insert_data():
+    # connect to MySQL
+    con = pymysql.connect(host='localhost', user='root', passwd='', db='testjson')
+    cursor = con.cursor()
 
-for i, item in enumerate(json_obj):
-    person = validate_string(item.get("person", None))
-    year = validate_string(item.get("year", None))
-    company = validate_string(item.get("company", None))
+    for i, item in enumerate(json_obj):
+        person = validate_string(item.get("person", None))
+        year = validate_string(item.get("year", None))
+        company = validate_string(item.get("company", None))
+
+        cursor.execute("INSERT INTO testp (person,	year,	company) VALUES (%s,	%s,	%s)",
+                       (person, year, company))
+    con.commit()
+    con.close()
 
 
-cursor.execute("INSERT INTO testp (person,	year,	company) VALUES (%s,	%s,	%s))",
-                           (person, year, company))
+def choices():
+    print("Insetion base de données")
+    print("1 - Afficher les données")
+    print("2 - Insertion des données")
+    print("3 - Fermer l'interface")
 
 
+def view_data():
+    with open(file, "r") as f:
+        temp = json.load(f)
+        for entry in temp:
+            person = entry["person"]
+            year = entry["year"]
+            company = entry["company"]
+            print(f"le nom : {person}")
+            print(f"l annee : {year}")
+            print(f"la compagnie : {company}")
+            print("\n\n")
 
-con.commit()
-con.close()
+
+while True:
+    choices()
+    choice = input("entrez un numero :")
+    if choice == "1":
+        view_data()
+    elif choice == "2":
+        insert_data()
+    elif choice == "3":
+        break
+    else:
+        print("Erreur")
